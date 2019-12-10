e0b569bb0743bfe263e2919955474fa2e684ea8e
Léo Couffinhal <leo.couffinhal@epitech.eu>
2019-12-10 01:15:00 +0100

    fix deprecation

--- src/Repository/ParacliniqueRepository.php
+++ src/Repository/ParacliniqueRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Paraclinique;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Paraclinique|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ParacliniqueRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Paraclinique::class);
     }

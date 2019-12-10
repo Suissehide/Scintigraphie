bb7d9dabffb2db840220bc8f16f8e30cbb31515c
Léo Couffinhal <leo.couffinhal@epitech.eu>
2019-12-10 03:17:00 +0100

    roles fix

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Form\FormError;
 
 /**
  * @Route("/")
